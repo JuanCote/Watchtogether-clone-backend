de9e27f653998441cb3f4dec57b07102ec47421f
JuanCote <lyy1707@gmail.com>
2022-07-15 22:53:40 +0300

    more cors

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from flask_socketio import SocketIO, emit, send, join_room, leave_room, close_ro
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"*": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'secret!'
-app.config['CORS_HEADERS'] = 'Content-Type'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 rooms = dict()
